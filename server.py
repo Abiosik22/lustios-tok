42682bccae6d48fcab927b4072b588aaa17c0a9b
Abiosik22 <155393973+Abiosik22@users.noreply.github.com>
2025-11-30 20:12:47 +0200

    Update server.py

--- server.py
+++ server.py
@@ -193,5 +193,7 @@ def get_feed():
 
 if __name__ == "__main__":
     import uvicorn
+    
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
-    uvicorn.run(app, host="0.0.0.0", port=8001)
